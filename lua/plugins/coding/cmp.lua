5dfc6cda7589e7a71e4e3ea6732f818e516a4f0a
HomelabX_GH <140975542+homelabX@users.noreply.github.com>
2024-06-25 23:55:58 +0530

    chore formatting

--- lua/plugins/coding/cmp.lua
+++ lua/plugins/coding/cmp.lua
@@ -1,18 +1,17 @@
 return {
-    'hrsh7th/nvim-cmp',
-    dependencies = {
-      			-- nvim-cmp source for neovim builtin LSP client
-			'hrsh7th/cmp-nvim-lsp',
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    -- nvim-cmp source for neovim builtin LSP client
+    "hrsh7th/cmp-nvim-lsp",
 
-      		-- nvim-cmp source for buffer words
-			'hrsh7th/cmp-buffer',
+    -- nvim-cmp source for buffer words
+    "hrsh7th/cmp-buffer",
 
-      			-- nvim-cmp source for path
-			'hrsh7th/cmp-path',
+    -- nvim-cmp source for path
+    "hrsh7th/cmp-path",
 
-      			-- nvim-cmp source for emoji
-			'hrsh7th/cmp-emoji',
-      'hrsh7th/cmp-cmdline',
-      
-    }
+    -- nvim-cmp source for emoji
+    "hrsh7th/cmp-emoji",
+    "hrsh7th/cmp-cmdline",
+  },
 }
