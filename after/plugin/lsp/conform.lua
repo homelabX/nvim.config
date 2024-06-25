37a7c7f6e152d2c5fd311ebcd2ef6c2a20b0e1dc
HomelabX_GH <140975542+homelabX@users.noreply.github.com>
2024-06-25 16:33:03 +0530

    separate lsp and formatter setup

--- after/plugin/lsp/conform.lua
+++ after/plugin/lsp/conform.lua
@@ -1,9 +1,6 @@
 local conform = require("conform")
 
 conform.setup({
-  formatters_by_ft = {
-    lua = { "stylua" },
-  },
   format_on_save = {
     -- These options will be passed to conform.format()
     timeout_ms = 500,
--- after/plugin/lsp/languages/lua.lua
+++ after/plugin/lsp/languages/lua.lua
@@ -1,3 +1,12 @@
 local lspconfig = require("lspconfig")
+local conform = require("conform")
 
+-- Formatter Setup
+conform.setup({
+    formatters_by_ft = {
+      lua = { "stylua" },
+    },
+})
+
+-- LSP Server Setup
 lspconfig.lua_ls.setup({})
