e09ad0c3800e5523a7810e6433993409b11d0033
HomelabX_GH <140975542+homelabX@users.noreply.github.com>
2024-06-27 13:05:37 +0530

    configure lualine

--- lua/homelab/plugins/lualine.lua
+++ lua/homelab/plugins/lualine.lua
@@ -1,4 +1,16 @@
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    local lualine = require("lualine")
+
+lualine.setup({
+  options = {
+    theme = "catppuccin",
+    component_separators = "",
+    section_separators = "",
+  },
+})
+
+end
 }
