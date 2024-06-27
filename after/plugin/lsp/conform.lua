4d044385d8001f359a08f3e9e234dc4c269d76a9
HomelabX_GH <140975542+homelabX@users.noreply.github.com>
2024-06-27 12:56:23 +0530

    install conform

--- after/plugin/lsp/conform.lua
+++
@@ -1,9 +0,0 @@
-local conform = require("conform")
-
-conform.setup({
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_format = "fallback",
-  },
-})
